e2498995a9990ffe48752adb728cad02b2174dcd
Ganendra Satya Sindhunata <ganendrasatyaa01@gmail.com>
2026-03-30 22:06:03 +0700

    Update Sesi.java

--- TugasKelompokPBO/src/Sesi.java
+++ TugasKelompokPBO/src/Sesi.java
@@ -1,1 +1,35 @@
+import java.util.Date;
 
+public class Sesi {
+    private String idSesi;
+    private Date tanggal;
+    private int durasi;
+
+    public Sesi(String idSesi, Date tanggal, int durasi) {
+        this.idSesi = idSesi;
+        this.tanggal = tanggal;
+        this.durasi = durasi;
+    }
+
+    public void mulaiSesi() {
+        assert idSesi != null : "ID Sesi belum di-set!";
+        System.out.println("Sesi " + idSesi + " dimulai.");
+    }
+
+    public void akhiriSesi() {
+        System.out.println("Sesi " + idSesi + " berakhir.");
+    }
+
+    public void absen(String namaPeserta) {
+        if (namaPeserta == null) throw new IllegalArgumentException("Nama kosong!");
+        System.out.println(namaPeserta + " hadir.");
+    }
+
+    public void absen(int idPeserta) {
+        System.out.println("ID " + idPeserta + " hadir.");
+    }
+
+    public String getRekap() {
+        return "Sesi: " + idSesi + " (" + durasi + " min)";
+    }
+}
